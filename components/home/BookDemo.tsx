7a0098bd5f5b4e0402acb05fd0bc7932542d0ad7
whashishkumar <wh.ashishkumar@gmail.com>
2026-01-13 12:33:22 +0530

    working on landing page

---
+++ components/home/BookDemo.tsx
@@ -0,0 +1,36 @@
+import SectionBadge from '../common/SectionBadge';
+import SectionHeading from '../common/SectionHeading';
+import AppointmentForm from '../common/AppointmentForm';
+import Image from 'next/image';
+
+export default function BookDemo() {
+  return (
+    <div className="bg-color">
+      <div className="inner-wrapper m-auto lg:py:16 py-12  lg:px-0 px-4">
+        <div className="flex justify-left">
+          <SectionBadge label="Book a Demo" icon="/icons/bone.svg" />
+        </div>
+        <SectionHeading
+          cssClass="text-left"
+          title={'Please Book a Demo Now!'}
+          subTitle={
+            'Need some advice or have any questions about our services? Contact us in any convenient way'
+          }
+          subHeadingCss="lg:width-[38rem]"
+        />
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-12 mt-12">
+          <div className="relative w-full h-[220px] sm:h-[280px] md:h-[340px] lg:h-[490px]">
+            <Image
+              src="/images/Webp/demo-img.webp"
+              alt="banner"
+              fill
+              className="object-fill rounded-2xl"
+              priority
+            />
+          </div>
+          <AppointmentForm />
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ components/home/QuickAnswers.tsx
@@ -0,0 +1,85 @@
+import React from 'react';
+import SectionBadge from '../common/SectionBadge';
+import SectionHeading from '../common/SectionHeading';
+import Accordion from '../ui/Accordian';
+import Image from 'next/image';
+
+export const faqData = [
+  {
+    id: 1,
+    question: 'How can I track critical and emergency patients in real time?',
+    answer:
+      'You can track critical and emergency patients in real time through Nurovet’s centralized dashboard, which highlights urgent cases with priority alerts, live status updates, and detailed patient profiles—ensuring faster decisions and timely care.',
+  },
+  {
+    id: 2,
+    question: 'Does the system provide alerts for urgent cases?',
+    answer:
+      'Yes, the system provides real-time alerts and notifications for all urgent and high-priority cases.',
+  },
+  {
+    id: 3,
+    question: 'How can I track critical and emergency patients in real time?',
+    answer:
+      'You can track critical and emergency patients in real time through Nurovet’s centralized dashboard, which highlights urgent cases with priority alerts, live status updates, and detailed patient profiles—ensuring faster decisions and timely care.',
+  },
+  {
+    id: 4,
+    question: 'Does the system provide alerts for urgent cases?',
+    answer:
+      'Yes, the system provides real-time alerts and notifications for all urgent and high-priority cases.',
+  },
+  {
+    id: 5,
+    question: 'How can I track critical and emergency patients in real time?',
+    answer:
+      'You can track critical and emergency patients in real time through Nurovet’s centralized dashboard, which highlights urgent cases with priority alerts, live status updates, and detailed patient profiles—ensuring faster decisions and timely care.',
+  },
+];
+
+export default function QuickAnswers() {
+  return (
+    <div
+      className="
+    bg-color
+  "
+    >
+      <div
+        className="
+    absolute
+    hidden
+    lg:block
+    lg:w-[15vw] xl:w-[20vw]
+    lg:h-45
+    bg-[url('/images/Webp/FAQRabit.webp')]
+    bg-no-repeat
+    bg-contain
+    overflow-hidden
+      "
+      />
+      <div className="inner-wrapper m-auto lg:py:16 py-12  lg:px-0 px-4">
+        <div className="flex justify-left">
+          <SectionBadge label="quick answers" icon="/icons/bone.svg" />
+        </div>
+        <SectionHeading cssClass="text-left" title={'Question Ask by Veterinarian'} />
+        <div className="grid grid-cols-1  md:grid-cols-2 gap-8">
+          <div className="pt-6">
+            <Accordion data={faqData} />
+          </div>
+          <div className="relative w-full h-[260px] sm:h-[300px] md:h-[380px] lg:h-[490px]">
+            <Image
+              src="/images/Webp/faq-img.webp"
+              alt="banner"
+              fill
+              sizes="(max-width: 640px) 100vw,
+           (max-width: 1024px) 80vw,
+           644px"
+              className="rounded-3xl object-fill sm:object-contain"
+              priority
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
