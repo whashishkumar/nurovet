7505ab642d2135e6d23e6497bc27f1ccfa681bca
whashishkumar <wh.ashishkumar@gmail.com>
2026-02-11 09:46:27 +0530

    fix banner spacing

--- components/sections/NewHeroBanner.tsx
+++ components/sections/NewHeroBanner.tsx
@@ -31,49 +31,50 @@ export default function NewHeroBanner({ heroBanner }: any) {
   } = heroContent || {}
 
 
-
   return (
-    <section className="relative overflow-hidden bg-[#00603A] min-h-auto lg:min-h-[90vh] flex items-center">
-      <div
-        className="absolute hidden md:block right-0 top-0 h-full w-[60vw] bg-no-repeat bg-contain bg-right-bottom"
-        style={{ backgroundImage: `url(${backgroundImage})` }}
-      />
-      <div className="relative z-10 inner-wrapper mx-auto px-6 lg:px-0 w-full  py-20">
-        <div className="max-w-3xl text-white ">
-          {title && <p className="mt-15 md:mt-25  text-white text-[2rem] lg:text-[3.125rem] font-bold leading-tight fredoka ">
-            {title?.first}
-          </p>}
-          <ul className="mt-8 space-y-4">
-            {features?.map((feature: string, index: number) => (
-              <li
-                key={index}
-                className="font-semibold text-lg lg:text-xl flex items-center"
-              >
-                <BsCheck2All className="w-6 h-6 mr-3 text-white" />
-                {feature}
-              </li>
-            ))}
-          </ul>
+    <div className="mx-auto sm:px-6 p-4 sm:py-6">
+      <section className="relative overflow-hidden bg-[#00603A] min-h-auto lg:min-h-[90vh] flex items-center  rounded-2xl">
+        <div
+          className="absolute hidden md:block right-0 top-0 h-full w-[60vw] bg-no-repeat bg-contain bg-right-bottom"
+          style={{ backgroundImage: `url(${backgroundImage})` }}
+        />
+        <div className="relative z-10 inner-wrapper mx-auto px-6 lg:px-0 w-full  py-20">
+          <div className="max-w-3xl text-white ">
+            {title && <p className="mt-15 md:mt-25  text-white text-[2rem] lg:text-[3.125rem] font-bold leading-tight fredoka ">
+              {title?.first}
+            </p>}
+            <ul className="mt-8 space-y-4">
+              {features?.map((feature: string, index: number) => (
+                <li
+                  key={index}
+                  className="font-semibold text-lg lg:text-xl flex items-center"
+                >
+                  <BsCheck2All className="w-6 h-6 mr-3 text-white" />
+                  {feature}
+                </li>
+              ))}
+            </ul>
 
-          <div className="mt-10 flex flex-wrap gap-4">
-            <Button
-              href={cta?.
-                href
-              }
-              label={cta?.label}
-              className="bg-white !text-black hover:bg-opacity-90 px-8 py-3 rounded-full font-normal transition-all hover:!text-white"
-            />
-            <Button
-              href={demobtn?.href}
-              label={demobtn?.label}
-              variant="outline"
-              className="border-white text-white hover:bg-white hover:!text-black px-8 py-3 rounded-full font-normal transition-all"
-            />
+            <div className="mt-10 flex flex-wrap gap-4">
+              <Button
+                href={cta?.
+                  href
+                }
+                label={cta?.label}
+                className="bg-white !text-black hover:bg-opacity-90 px-8 py-3 rounded-full font-normal transition-all hover:!text-white"
+              />
+              <Button
+                href={demobtn?.href}
+                label={demobtn?.label}
+                variant="outline"
+                className="border-white text-white hover:bg-white hover:!text-black px-8 py-3 rounded-full font-normal transition-all"
+              />
+            </div>
+
           </div>
-
         </div>
-      </div>
 
-    </section>
+      </section>
+    </div>
   );
 }
