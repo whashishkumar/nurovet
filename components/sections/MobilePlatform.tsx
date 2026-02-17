7a29bbac848ef39b2f05f8fcb6ff1498d1eb351c
whashishkumar <wh.ashishkumar@gmail.com>
2026-02-17 14:42:47 +0530

    fix btn width

--- components/sections/MobilePlatform.tsx
+++ components/sections/MobilePlatform.tsx
@@ -26,38 +26,40 @@ const MobilePlatform = ({ mobileSection }: any) => {
   const { description, stores, phones, heading } = mobileSection || {};
   return (
     <section className="relative bg-[#00603A] overflow-hidden ">
-      <div className="max-w-[1216px] m-auto grid grid-cols-1 lg:grid-cols-2  items-center lg:gap-12 px-6 lg:px-0">
-        <div className="text-white py-10 lg:py-12 text-center lg:text-left ">
-          <h1 className="text-[2rem] lg:text-[3rem] font-medium lg:leading-15 whitespace-pre-line onesta">
-            {heading}
-          </h1>
-          <p className="mt-6 text-white text-lg leading-relaxed onesta">{description}</p>
-          <div className="mt-4">
-            <p className="mb-4 font-medium onesta text-lg lg:leading-7">Get the App</p>
-            <div className="flex gap-4 justify-center lg:justify-start">
-              {stores?.map((store: any, i: number) => (
-                <Image
-                  key={i}
-                  {...store}
-                  height={40}
-                  width={135}
-                  alt="playstore"
-                  className="object-contain"
-                />
-              ))}
+      <div className="max-w-[76rem] m-auto">
+        <div className=" grid grid-cols-1 lg:grid-cols-2  items-center lg:gap-12 px-6 lg:px-0">
+          <div className="text-white py-10 lg:py-12 text-center lg:text-left ">
+            <h1 className="text-[2rem] lg:text-[3rem] font-medium lg:leading-15 whitespace-pre-line onesta">
+              {heading}
+            </h1>
+            <p className="mt-6 text-white text-lg leading-relaxed onesta">{description}</p>
+            <div className="mt-4">
+              <p className="mb-4 font-medium onesta text-lg lg:leading-7">Get the App</p>
+              <div className="flex gap-4 justify-center lg:justify-start">
+                {stores?.map((store: any, i: number) => (
+                  <Image
+                    key={i}
+                    {...store}
+                    height={40}
+                    width={135}
+                    alt="playstore"
+                    className="object-contain"
+                  />
+                ))}
+              </div>
             </div>
           </div>
-        </div>
-        <div className="relative hidden lg:flex justify-center items-center h-full">
-          {phones?.left?.src && (
-            <Image
-              src={phones?.left?.src}
-              alt="banner-img"
-              width={520}
-              height={558}
-              className="relative object-contain w-full h-full"
-            />
-          )}
+          <div className="relative hidden lg:flex justify-center items-center h-full">
+            {phones?.left?.src && (
+              <Image
+                src={phones?.left?.src}
+                alt="banner-img"
+                width={520}
+                height={558}
+                className="relative object-contain w-full h-full"
+              />
+            )}
+          </div>
         </div>
       </div>
     </section>
